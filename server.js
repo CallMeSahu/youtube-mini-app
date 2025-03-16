0ea92a91ca9ef8f77b5c8bd37233ba6367625ac0
Siddhartha Sahu <siddharthasahu.work@gmail.com>
2025-03-16 11:48:36 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const videoRoutes = require('./routes/videoRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.set('view engine', 'ejs');
+
+app.use('/', videoRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
